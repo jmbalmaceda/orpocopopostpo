c82e51b9444c2200c9f50e0408e5dd177ac235ab
jmbalmaceda@gmail.com <jmbalmaceda@gmail.com>
2017-01-12 01:32:58 -0300

    Agregado procesamiento y datos de Toques

---
+++ InformeHtml/series/dataTocaPorMes.js
@@ -0,0 +1,213 @@
+var seriesTocaPorMes = [ {
+    "data" : [ {
+      "y" : 5,
+      "x" : Date.UTC(2016,8,1)
+    }, {
+      "y" : 3,
+      "x" : Date.UTC(2016,9,1)
+    }, {
+      "y" : 12,
+      "x" : Date.UTC(2016,10,1)
+    }, {
+      "y" : 9,
+      "x" : Date.UTC(2016,11,1)
+    } ],
+    "type" : "spline",
+    "name" : "Lysoform"
+  }, {
+    "data" : [ {
+      "y" : 1,
+      "x" : Date.UTC(2016,8,1)
+    }, {
+      "y" : 1,
+      "x" : Date.UTC(2016,9,1)
+    } ],
+    "type" : "spline",
+    "name" : "Lysoform Pocket"
+  }, {
+    "data" : [ ],
+    "type" : "spline",
+    "name" : "Lysoform automatico"
+  }, {
+    "data" : [ {
+      "y" : 1,
+      "x" : Date.UTC(2016,9,1)
+    }, {
+      "y" : 1,
+      "x" : Date.UTC(2016,11,1)
+    } ],
+    "type" : "spline",
+    "name" : "Ayudin"
+  }, {
+    "data" : [ {
+      "y" : 1,
+      "x" : Date.UTC(2016,8,1)
+    } ],
+    "type" : "spline",
+    "name" : "Espadol"
+  }, {
+    "data" : [ {
+      "y" : 4,
+      "x" : Date.UTC(2016,8,1)
+    }, {
+      "y" : 2,
+      "x" : Date.UTC(2016,10,1)
+    }, {
+      "y" : 1,
+      "x" : Date.UTC(2016,11,1)
+    } ],
+    "type" : "spline",
+    "name" : "Poett"
+  }, {
+    "data" : [ {
+      "y" : 3,
+      "x" : Date.UTC(2016,8,1)
+    }, {
+      "y" : 1,
+      "x" : Date.UTC(2016,9,1)
+    } ],
+    "type" : "spline",
+    "name" : "Glade"
+  }, {
+    "data" : [ {
+      "y" : 5,
+      "x" : Date.UTC(2016,8,1)
+    }, {
+      "y" : 1,
+      "x" : Date.UTC(2016,9,1)
+    }, {
+      "y" : 3,
+      "x" : Date.UTC(2016,10,1)
+    }, {
+      "y" : 19,
+      "x" : Date.UTC(2016,11,1)
+    } ],
+    "type" : "spline",
+    "name" : "Glade chico 2600 aplic."
+  }, {
+    "data" : [ {
+      "y" : 8,
+      "x" : Date.UTC(2016,8,1)
+    }, {
+      "y" : 3,
+      "x" : Date.UTC(2016,9,1)
+    }, {
+      "y" : 4,
+      "x" : Date.UTC(2016,11,1)
+    } ],
+    "type" : "spline",
+    "name" : "New Scent"
+  }, {
+    "data" : [ {
+      "y" : 1,
+      "x" : Date.UTC(2016,9,1)
+    } ],
+    "type" : "spline",
+    "name" : "Poett aroma sensations (Cuello mas chico tapa blanca)"
+  }, {
+    "data" : [ {
+      "y" : 1,
+      "x" : Date.UTC(2016,8,1)
+    }, {
+      "y" : 1,
+      "x" : Date.UTC(2016,10,1)
+    }, {
+      "y" : 3,
+      "x" : Date.UTC(2016,11,1)
+    } ],
+    "type" : "spline",
+    "name" : "Poett Rociador Natural"
+  }, {
+    "data" : [ {
+      "y" : 7,
+      "x" : Date.UTC(2016,8,1)
+    }, {
+      "y" : 2,
+      "x" : Date.UTC(2016,9,1)
+    }, {
+      "y" : 2,
+      "x" : Date.UTC(2016,10,1)
+    }, {
+      "y" : 8,
+      "x" : Date.UTC(2016,11,1)
+    } ],
+    "type" : "spline",
+    "name" : "Atomizadores en general"
+  }, {
+    "data" : [ {
+      "y" : 2,
+      "x" : Date.UTC(2016,8,1)
+    }, {
+      "y" : 1,
+      "x" : Date.UTC(2016,9,1)
+    }, {
+      "y" : 6,
+      "x" : Date.UTC(2016,11,1)
+    } ],
+    "type" : "spline",
+    "name" : "Sensations Glade (Cuadrado)"
+  }, {
+    "data" : [ {
+      "y" : 1,
+      "x" : Date.UTC(2016,9,1)
+    }, {
+      "y" : 1,
+      "x" : Date.UTC(2016,10,1)
+    } ],
+    "type" : "spline",
+    "name" : "Aceite Natural Glade"
+  }, {
+    "data" : [ ],
+    "type" : "spline",
+    "name" : "Lata Aromática Gel"
+  }, {
+    "data" : [ {
+      "y" : 2,
+      "x" : Date.UTC(2016,8,1)
+    }, {
+      "y" : 1,
+      "x" : Date.UTC(2016,9,1)
+    }, {
+      "y" : 6,
+      "x" : Date.UTC(2016,11,1)
+    } ],
+    "type" : "spline",
+    "name" : "Aromatizadores para el Auto"
+  }, {
+    "data" : [ {
+      "y" : 6,
+      "x" : Date.UTC(2016,8,1)
+    }, {
+      "y" : 1,
+      "x" : Date.UTC(2016,9,1)
+    }, {
+      "y" : 3,
+      "x" : Date.UTC(2016,10,1)
+    }, {
+      "y" : 2,
+      "x" : Date.UTC(2016,11,1)
+    } ],
+    "type" : "spline",
+    "name" : "Artefacto Spray Automatico"
+  }, {
+    "data" : [ {
+      "y" : 1,
+      "x" : Date.UTC(2016,9,1)
+    }, {
+      "y" : 2,
+      "x" : Date.UTC(2016,11,1)
+    } ],
+    "type" : "spline",
+    "name" : "Air Wick"
+  }, {
+    "data" : [ ],
+    "type" : "spline",
+    "name" : "Poett Home style (Palitos caja)"
+  }, {
+    "data" : [ {
+      "y" : 1,
+      "x" : Date.UTC(2016,10,1)
+    } ],
+    "type" : "spline",
+    "name" : "Otros"
+  } ];
